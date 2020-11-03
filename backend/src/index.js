6f5a5e3081d9658bbeb8b2103df713fba8153eca
Valdir Torres <valdyrtorres@yahoo.com.br>
2020-11-02 21:58:03 -0300

    fim da parte 'Back-end com Node.js'

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -23,7 +23,39 @@ app.use(express.json()); // deve vir aqui, devido a linearidade do express
     Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
  */
 
+ /*
+    Middleware:
+
+    É um interceptador de requisições que interrompe totalmente a requisição ou alterar dados da requisição
+    todo módulo express é baseado em middlewares
+ */
+
 const projects = [];
+
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.time(logLabel);
+
+    next(); // Próximo middleware
+
+    console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID.'});
+    }
+
+    return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
     const { title } = request.query;
